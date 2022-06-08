3deee137180139077b607c0bb3322ea3a9bdd75b
yulia633 <ignat633yulia@yandex.ru>
2022-06-08 22:54:34 +0300

    add twig

--- public/index.php
+++ public/index.php
@@ -5,19 +5,28 @@ declare(strict_types=1);
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$loader = new FilesystemLoader('../templates');
+$twig = new Environment($loader);
 
 // Create app
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) use ($twig) {
+    $body = $twig->render('index.html.twig');
+    $response->getBody()->write($body);
     return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("About page!");
+$app->get('/about', function (Request $request, Response $response, $args) use ($twig) {
+    $body = $twig->render('about.html.twig', [
+        'name' => 'Test'
+    ]);
+    $response->getBody()->write($body);
     return $response;
 });
 
