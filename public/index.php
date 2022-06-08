44e6106c7df5192d02833d94f3ab73332563f2ee
yulia633 <ignat633yulia@yandex.ru>
2022-06-08 22:10:04 +0300

    add index

--- public/index.php
+++ public/index.php
@@ -2,4 +2,23 @@
 
 declare(strict_types=1);
 
-echo "blog";
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Create app
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->get('/about', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("About page!");
+    return $response;
+});
+
+$app->run();
